552ef0f2064dcc2fdb73d44a244439bb3dcddfc9
Jon Hawkesworth <jhawkesworth@users.noreply.github.com>
2023-03-24 15:26:01 +0000

    moved a lot of stuff around and took out any attempt to use the rocket FileServer.
    This breaks the htmx stuff but I'm going to commit as I cannot get deploy to complete as is.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use rocket_dyn_templates::Template;
 use rocket_dyn_templates::context;
 use rocket::response::Redirect;
 use rocket::Request;
-use rocket::fs::{FileServer};
+//use rocket::fs::{FileServer};
 
 #[get("/")]
 fn index() -> Redirect {
@@ -48,14 +48,15 @@ pub fn not_found(req: &Request<'_>) -> Template {
 }
 
 #[shuttle_runtime::main]
-//async fn rocket(#[shuttle_static_folder::StaticFolder(folder = "templates")] _static_folder: PathBuf) -> shuttle_service::ShuttleRocket {
-async fn rocket(#[shuttle_static_folder::StaticFolder(folder = "templates")] static_folder: PathBuf) ->  shuttle_rocket::ShuttleRocket {
-    let static_path = static_folder.as_path();
+async fn rocket(#[shuttle_static_folder::StaticFolder] _static_folder: PathBuf) -> shuttle_rocket::ShuttleRocket {
+    //let static_path = static_folder.as_path();
+    //std::thread::sleep(std::time::Duration::from_millis(3000));
     //    static_subdir.push("static");
+    //let static = static_folder.clone();
     let rocket = rocket::build()
         .mount("/", routes![index, hello, button_clicked, a_clicked, favicon])
-        .mount("/static", FileServer::from(static_path) )
-        //.mount("/hello/static", FileServer::from(relative!("templates")) )
+//        .mount("/static", FileServer::from(static.as_path()) )
+        //.mount("/hello/static", FileServer::from(relative!("static")) )
         // .mount("/static", FileServer::from(relative!(&static_subdir)) )
         // .mount("/hello/static", FileServer::from(relative!(&static_subdir)) )
         .register("/", catchers![not_found])
