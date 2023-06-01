9e64765cdda0ddd13868c498476b9a5f806d7a8e
Jon Hawkesworth <jhawkesworth@users.noreply.github.com>
2023-06-01 10:10:30 +0100

    Got basic crud working, some of it via htmx, although add is currently a regular form submission (boo!).
    Much cleanup to be done.

---
+++ schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE if not exists todos (
+                       id serial PRIMARY KEY,
+                       note TEXT NOT NULL
+);
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,97 @@
 #[macro_use]
 extern crate rocket;
 use std::path::PathBuf;
+
 use rocket_dyn_templates::Template;
 use rocket_dyn_templates::context;
+
 use rocket::response::Redirect;
 use rocket::Request;
 use rocket::fs::{FileServer, relative};
+use rocket::serde::Serialize;
+use rocket::State;
+use rocket::form::Form;
+
+use sqlx::{Executor, FromRow, PgPool};
+
+use shuttle_runtime::CustomError;
+
+struct MyState {
+    pool: PgPool,
+}
+
+#[derive(FromRow, Serialize, FromForm)]
+struct Todo {
+    pub id: i32,
+    pub note: String,
+}
+
+
+
+#[derive(FromForm)]
+struct TodoNew{
+    pub note: String,
+}
+
+#[get("/todos")]
+async fn retrieve_todos_page(state: &State<MyState>) -> Template {
+    let default_todo = Todo{ id: -1_i32,  note: "Add some things to do!".to_string() };
+    // let items = sqlx::query_as!(Todo, "SELECT * from todos LIMIT 10")
+    //     .fetch_all(&state.pool)
+    //     .await.
+    //     unwrap_or(vec![default_todo]);
+    Template::render("index", context! {
+        title: "Todo List",
+        name: Some("Not used at the moment"),
+        items: sqlx::query_as!(Todo, "SELECT * from todos LIMIT 10")
+        .fetch_all(&state.pool)
+        .await.
+        unwrap_or(vec![default_todo]),
+    })
+}
+
+#[post("/todos", data = "<data>")]
+async fn add(data: Form<TodoNew>, state: &State<MyState>) -> Redirect {
+    let note = &data.note;
+    let _todo = sqlx::query_as!(TodoNew,
+        "INSERT INTO todos(note) VALUES ($1) RETURNING note",
+        note.as_str())
+        // .bind(note.as_str())
+        .fetch_one(&state.pool)
+        .await.unwrap_or(TodoNew {note: "oh we failed".to_string()});
+    // it is possible above insert should be written using 'execute' isntead of 'fetch_one'
+    // TODO work out what htmx failure pattern should be.
+
+    Redirect::to(uri!("/todos"))
+}
+
+#[get("/todos/<id>/edit")]
+async fn edit_todo_form(id: i32, state: &State<MyState>) -> Template {
+    let default_todo = Todo{ id: -1_i32,  note: "Add some things to do!".to_string() };
+    let item = sqlx::query_as!(Todo, "SELECT * from todos where id= ($1)", id)
+        .fetch_one(&state.pool)
+        .await.
+        unwrap_or(default_todo);
+    Template::render("edit_todo", context! {
+        title: "Edit todo",
+        item: item
+    })
+}
+#[put("/todos/<id>", data = "<data>")]
+async fn put_todo(data: Form<Todo>, id: i32, state: &State<MyState>) -> Template {
+    // TODO a boat load in tests regarding input.
+    let default_todo = Todo{ id: -1_i32,  note: "Add some things to do!".to_string() };
+    let item = sqlx::query_as!(Todo, "update TODOS set note=($1) where id= ($2) returning *", data.note, id)
+        .fetch_one(&state.pool)
+        .await.
+        unwrap_or(default_todo);
+
+    Template::render("saved_todo", context! {
+        title: "Saved todo",
+        item: item
+    })
+}
+
 
 #[get("/")]
 fn index() -> Redirect {
@@ -26,6 +112,9 @@ pub fn hello(name: &str) -> Template {
         items: vec!["One", "Two", "Three"],
     })
 }
+
+
+
 #[post("/clicked")]
 pub fn button_clicked() -> Template {
     Template::render("no_button", context! {
@@ -48,18 +137,23 @@ pub fn not_found(req: &Request<'_>) -> Template {
 }
 
 #[shuttle_runtime::main]
-async fn rocket(#[shuttle_static_folder::StaticFolder] static_folder: PathBuf) -> shuttle_rocket::ShuttleRocket {
+async fn rocket(#[shuttle_shared_db::Postgres] pool: PgPool, #[shuttle_static_folder::StaticFolder] static_folder: PathBuf) -> shuttle_rocket::ShuttleRocket {
+
+    pool.execute(include_str!("../schema.sql"))
+        .await
+        .map_err(CustomError::new)?;
+
+    let state = MyState { pool };
+
     let template_dir = static_folder.to_str().unwrap();
     let figment = rocket::Config::figment().merge(("template_dir", template_dir));
-    let rocket = rocket::custom(figment).mount("/", routes![index, hello, button_clicked, a_clicked, favicon])
-//        .mount("/static", FileServer::from(static.as_path()) )
-        //.mount("/hello/static", FileServer::from(relative!("static")) )
-        // .mount("/static", FileServer::from(relative!(&static_subdir)) )
-        // .mount("/hello/static", FileServer::from(relative!(&static_subdir)) )
-        //.mount("/", FileServer::from(relative!("static")))
+    let rocket = rocket::custom(figment)
+        .mount("/", routes![index, hello, button_clicked, a_clicked, favicon,
+            retrieve_todos_page, add, edit_todo_form, put_todo])
         .mount("/static", FileServer::from(relative!("static")))
         .register("/", catchers![not_found])
         .attach(Template::fairing())
+        .manage(state)
         ;
 
     Ok(rocket.into())
